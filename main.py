7cda80add0a2bd7c63eb375da4ca5eed684f8924
fox <o.citkowski@gmail.com>
2022-03-10 17:45:45 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from locale import str
+
+var = 1
+var2 = "texy"
+var22 = type("texy")
+var3 = [1, "rr", 2]
+var4 = (1, "rr", 2)
+var5 = {"Van" : 2, "6" : 5}
+var6 = {1, 2, 3, 4, 4}
+
+
+print(str(var) + str(var))
+print(type(var22))
+print(var3)
+print(var4)
+print(var5)
+print(var6)
+
