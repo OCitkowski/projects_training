8c4491e06e60516ea0a6966793e9b434dc88baf9
fox <o.citkowski@gmail.com>
2022-03-17 15:07:05 +0200

    3

---
+++ exercise_001/e_01.py
@@ -0,0 +1,25 @@
+from builtins import print
+
+my_list = [1, 'jhf', 'jhfjhgfjh', 5, 8957, 456.2]
+print(my_list)
+print('start')
+sum_element = ''
+for element in my_list:
+    print(element)
+    if not type(element) == 'str':
+        element = str(element)
+        sum_element = f"sum_element - {sum_element + '---' + element}"
+        # print(sum_element)
+
+#count_sum_element = sum_element.count()
+#print(count_sum_element)
+print(f" {sum_element} --- {type(sum_element)}")
+my_new_list = sum_element .split()
+print(my_new_list)
+
+my_new_list2 = []
+for new_element in my_new_list:
+    if not new_element == '-':
+        my_new_list2.append(new_element)
+
+print(my_new_list2)
