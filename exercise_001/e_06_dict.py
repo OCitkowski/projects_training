2cacdb5af557cf7a2637ad5c07955db2ff7f224e
fox <o.citkowski@gmail.com>
2022-03-19 11:26:21 +0200

    6.1

--- exercise_001/e_06_dict.py
+++ exercise_001/e_06_dict.py
@@ -1,23 +1,48 @@
-alien_0 = {}
-alien_0['color'] = 'green'
+# alien_0 = {}
+# alien_0['color'] = 'green'
+# alien_0['zvert'] = '09_ff'
+# alien_0['color2'] = 'green'
+#
+# # alen_0 = {'color': 'green', 'points': 5}
+# # print(f" {alen_0}")
+# alien_0['x_position'] = 0
+# alien_0['y_position'] = 25
+# # print(f'{alien_0}')
+# alien_0['x_position'] = 444
+# alien_0['z_position'] = 87.5
+# # del alien_0['color']
+# # print(f"{alien_0}")
+# pointss = alien_0.get('clll', 'no points')
+# # print(f"{pointss}")
+#
+# for key, val in alien_0.items():
+#     print(f"key = {key},     val =  {val}")
+# print(f'**********01************')
+# for name in alien_0:
+#     print(f"{name} // {alien_0[name]}")
+# print(f'***********02***********')
+# for key in sorted(alien_0.keys()):
+#     print(f' {key}')
+# print(f'*********03*************')
+# for val in alien_0.values():
+#     print(f'***** {val}')
+# print(f'*********04*************')
+# my_set = set(alien_0.values())
+# print(f'{alien_0.values()}')
+# print(f'{my_set}')
+alients = []
+for alient_number in range(30):
+    new_alient = {'color': 'green', 'points': 100, 'speed': 200}
+    alients.append(new_alient)
+print(f' total alients = {len(alients)} {alients[0:2]}')
 
-# alen_0 = {'color': 'green', 'points': 5}
-# print(f" {alen_0}")
-alien_0['x_position'] = 0
-alien_0['y_position'] = 25
-# print(f'{alien_0}')
-alien_0['x_position'] = 444
-alien_0['z_position'] = 87.5
-# del alien_0['color']
-# print(f"{alien_0}")
-pointss = alien_0.get('clll', 'no points')
-# print(f"{pointss}")
-
-for key, val in alien_0.items():
-    print(f"key = {key},     val =  {val}")
-print(f'**********************')
-for name in alien_0:
-    print(f"{name} // {alien_0[name]}")
-print(f'**********************')
+i = len(alients)
+while i != 0:
+    i -= 1
+    if i%2 == 0:
+        alients[i]['color'] = 'red'
+        alients[i]['speed'] = 55
+for alient in alients:
+    print(alient)
 
 
