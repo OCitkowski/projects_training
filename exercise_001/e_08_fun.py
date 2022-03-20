247333b6156053f5a577a95c09204baa686cf259
fox <o.citkowski@gmail.com>
2022-03-20 10:10:09 +0200

    8.2

--- exercise_001/e_08_fun.py
+++ exercise_001/e_08_fun.py
@@ -1,24 +1,71 @@
+#
+# def city_country(contry, city):
+#     return f'{contry}, {city}'
+# city_country_formated = city_country('Ukraine', 'Kyiv')
+# print(city_country_formated)
+#
+# def make_album(first_name, last_name, middle_name, age, id = None):
+#
+#     if id:
+#         my_list = [first_name, last_name, middle_name, age, id ]
+#     else:
+#         my_list = [first_name, last_name, middle_name, age, "not have id"]
+#     my_dictionary = {}
+#     my_dictionary[first_name] = my_list
+#
+#     return my_dictionary
+#
+# formated_album = make_album('alex', 'ci', 'volod', '43')
+# print(formated_album)
+#
+# formated_album = make_album('alex', 'ci', 'volod', '43', 76654654564)
+# print(formated_album)
 
-def city_country(contry, city):
-    return f'{contry}, {city}'
-city_country_formated = city_country('Ukraine', 'Kyiv')
-print(city_country_formated)
+def get_input_list(phone_list= None):
 
-def make_album(first_name, last_name, middle_name, age, id = None):
+    if not phone_list:
+        phone_list = []
+    active = True
 
-    if id:
-        my_list = [first_name, last_name, middle_name, age, id ]
-    else:
-        my_list = [first_name, last_name, middle_name, age, "not have id"]
-    my_dictionary = {}
-    my_dictionary[first_name] = my_list
+    while active:
+        first_name = input('enter first name -\n')
+        if not first_name:
+            continue
+        elif first_name =='exit':
+            break
 
-    return my_dictionary
+        last_name = input('enter last name - \n')
+        if not last_name:
+            continue
+        elif last_name == 'exit':
+            break
 
-formated_album = make_album('alex', 'ci', 'volod', '43')
-print(formated_album)
+        phone = input('enter your phone number - \n')
+        if not phone:
+            continue
+        elif phone == 'exit':
+            break
 
-formated_album = make_album('alex', 'ci', 'volod', '43', 76654654564)
-print(formated_album)
+        tin = input('enter your TIN code - \n')
+        if not tin:
+            continue
+        elif tin == 'exit':
+            break
+
+        phone_list.append({phone: [first_name, last_name, tin]})
+
+    return phone_list
+
+main_phone_list = [{'34565656': ['afgfdg', 'dfghfdgh', '456546']}, {'45656': ['dsftghgfh', 'fhgh', '4565']}]
+main_phone_list2 = get_input_list(main_phone_list[:])
+
+for item in main_phone_list:
+    print(item)
+
+
+
+
+
+
 
 
