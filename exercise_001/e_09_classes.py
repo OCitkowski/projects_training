d368d5f4d2e01a7e944b9dbe8331bee0c059b826
fox <o.citkowski@gmail.com>
2022-03-20 19:15:32 +0200

    9.2

--- exercise_001/e_09_classes.py
+++ exercise_001/e_09_classes.py
@@ -1,4 +1,40 @@
+# ********************9.2***********
+class Car:
 
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometr = 0
+
+    def get_desccriptive_name(self):
+        """return formated name"""
+        long_name = f'{self.make} {self.year} {self.model}'
+        return long_name
+    def read_odometr(self):
+        """prin odometr value"""
+        print(f'this car has - {self.odometr}  km')
+    def update_odometr(self, milwage):
+        """set odometr value for car"""
+        if self.odometr <= milwage:
+            self.odometr = milwage
+        else:
+            print(f"dont touch")
+    def increment_odometr(self, miles):
+        """Add km to auto miles"""
+        self.odometr += miles
+
+
+my_new_car = Car("opel", "a4", 2022)
+my_new_car.odometr = 89
+print(my_new_car.get_desccriptive_name().title())
+my_new_car.read_odometr()
+my_new_car.update_odometr(5)
+my_new_car.read_odometr()
+my_new_car.increment_odometr(54)
+my_new_car.read_odometr()
+
+# ************************9.1*******
 class Dog:
     """Dog, is my dog"""
 
