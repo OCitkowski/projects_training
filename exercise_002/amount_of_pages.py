282630392b0c0bf3eba0d814af5511c36121aeff
fox <o.citkowski@gmail.com>
2022-04-08 10:33:44 +0300

    function amount_of_pages + best practice

--- exercise_002/amount_of_pages.py
+++ exercise_002/amount_of_pages.py
@@ -1,74 +1,43 @@
-result = None
-number_of_iterations = 0
+def best_amount_of_pages(summary):
+    # 1-9:          9       = 9 * 1 * 10**0
+    # 10-99:        180     = 9 * 2 * 10**1
+    # 100-999:      2700    = 9 * 3 * 10**2
+    # 1000-9999:    36000   = 9 * 4 * 10**3
+    # 10000-99999:  450000  = 9 * 5 * 10**4
+    res = 0
+    for x in range(1, 6):
+        y = 9 * x * 10**(x-1)
+        if summary <= y:
+            return res + summary//x
+        res += y//x
+        summary -= y
 
+def amount_of_pages(summary: int, len_sum: int = None) -> int:
+    calculate_old = lambda num: (len(''.join([str(i) for i in range(1, num + 1)])))
+    calc_string = f'({summary}'
+    len_sum = len(str(summary)) if len_sum == None else len_sum
+    for i in range(0, len_sum):
+        if i != len_sum - 1:
+            calc_string += ' - '
+            calc_string += '9' + '0' * i + '*' + f'{i + 1}'
+        else:
+            if len_sum == 1:
+                calc_string += ')'
+            else:
+                calc_string += f') // {len_sum} + {"9" * (len_sum - 1)}'
+    result = eval(calc_string)
 
-def amount_of_pages_old(summary: int, amound_dict: dict = None) -> int:
-    global result
-    global number_of_iterations
+    if summary != calculate_old(result):
+        result = amount_of_pages(summary, len_sum - 1)
 
-    computed = lambda num: (len(''.join([str(i) for i in range(1, num + 1)])))
-
-    if amound_dict == None:
-        amound_dict = {}
-        amound_dict['number_of_iterations'] = 0
-        amound_dict['num_min'] = summary // int(len(str(summary)))
-        amound_dict['num_max'] = summary if summary < 1000 else summary // 2
-        amound_dict['sum_min'] = computed(amound_dict['num_min'])
-        amound_dict['sum_max'] = computed(amound_dict['num_max'])
-
-    amound_dict['number_of_iterations'] += 1
-
-    if amound_dict['number_of_iterations'] == 10:
-        return result
-
-    num = (amound_dict['num_min'] + amound_dict['num_max']) // 2
-    sum = computed(num)
-
-    if sum == summary:
-        amound_dict['sum_min'] = sum
-        amound_dict['num_min'] = num
-        result = amound_dict['num_min']
-        print(f' iteration {amound_dict["number_of_iterations"]}')
-        print(
-            f' result {result} // summary {summary} '
-            f'// sum_min/sum_max - {amound_dict["sum_min"]} / {amound_dict["sum_max"]} ')
-        return result
-
-    if sum > summary:
-        amound_dict['num_max'] = num
-        amound_dict['sum_max'] = sum
-
-    if sum < summary:
-        amound_dict['sum_min'] = sum
-        amound_dict['num_min'] = num
-
-    amount_of_pages(summary, amound_dict)
-
+    print(f'{calc_string} ----- res {result} // sum {summary} == {calculate_old(result)}')
     return result
 
-def amount_of_pages(summary: int) -> int:
-    # calculate_old = lambda num: (len(''.join([str(i) for i in range(1, num + 1)])))
-    # calc_string = f'({summary}'
-    # len_sum = len(str(summary))
-    # for i in range(0, len_sum):
-    #     if i != len_sum - 1:
-    #         calc_string += ' - '
-    #         calc_string += '9' + '0' * i + '*' + f'{i + 1}'
-    #     else:
-    #         if len_sum == 1:
-    #             calc_string += ')'
-    #         else:
-    #             calc_string += f') // {len_sum} + {"9" * (len_sum - 1)}'
-    # result = eval(calc_string)
-
-    # print(f'{calc_string} ----- res {result} // sum {summary} == {calculate_old(result)}')
-    return None
-
-if __name__ == '__main__':                                                                                                                                                                                                                                                                                                        amount_of_pages(1095)
-    amount_of_pages(5)
+if __name__ == '__main__':
+    # amount_of_pages(1095)
     # assert amount_of_pages(5) == 5
-    # assert amount_of_pages(25) == 17
-    # assert amount_of_pages(185) == 97
-    # assert amount_of_pages(660) == 256
-    # assert amount_of_pages(1095) == 401
-    # assert amount_of_pages(2893) == 1000
+    assert amount_of_pages(25) == 17
+    assert amount_of_pages(185) == 97
+    assert amount_of_pages(660) == 256
+    assert amount_of_pages(1095) == 401
+    assert amount_of_pages(2893) == 1000
