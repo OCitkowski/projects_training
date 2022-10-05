32413aa4f8feae0ad15c1da52c816082a261d6bd
OCitkowski <o.citkowski@gmail.com>
2022-10-05 11:15:41 +0300

    + 21 solution end

--- exercise_002/ex_21_roman_numerals_encoder.py
+++ exercise_002/ex_21_roman_numerals_encoder.py
@@ -15,11 +15,31 @@
 # D          500
 # M          1,000
 
-def solution(n):
+def solution(arabic: int):
     # TODO convert int to roman string
-    roman = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50, 'XC': 90, 'C': 100, 'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
-    return 'I'
+    lookup = [
+        (1000, 'M'),
+        (900, 'CM'),
+        (500, 'D'),
+        (400, 'CD'),
+        (100, 'C'),
+        (90, 'XC'),
+        (50, 'L'),
+        (40, 'XL'),
+        (10, 'X'),
+        (9, 'IX'),
+        (5, 'V'),
+        (4, 'IV'),
+        (1, 'I'),
+    ]
+    res = ''
+    for (n, roman) in lookup:
+        (d, arabic) = divmod(arabic, n)
+        res += roman * d
+    return res
+
 
 
 if __name__ == '__main__':
-    assert solution(1) == 'I'
+    # solution(1947)
+    assert solution(1947) == 'MCMXLVII'
