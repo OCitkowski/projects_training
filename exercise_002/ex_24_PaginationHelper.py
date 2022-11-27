c0c8f032737776ce87d78b6d14e05e1d7f0830b3
OCitkowski <o.citkowski@gmail.com>
2022-11-27 18:38:32 +0100

    24+

--- exercise_002/ex_24_PaginationHelper.py
+++ exercise_002/ex_24_PaginationHelper.py
@@ -1,25 +1,43 @@
-# A simple calculator
-class Calculator:
-    # empty constructor
-    def __init__(self)
+
+
+class PaginationHelper:
+
+    # The constructor takes in an array of items and a integer indicating
+    # how many items fit within a single page
+    def __init__(self, collection, items_per_page):
+        self.collection = collection
+        self.items_per_page = items_per_page
+        self.page_index = -1
+        self.page_item_count = -1
+
+    # returns the number of items within the entire collection
+    def item_count(self):
+        return len(self.collection)
+
+    # returns the number of pages
+    def page_count(self):
+        if len(self.collection) % self.items_per_page == 0:
+            return len(self.collection) // self.items_per_page
+        else:
+            return len(self.collection) // self.items_per_page + 1
+
+    # returns the number of items on the current page. page_index is zero based
+    # this method should return -1 for page_index values that are out of range
+    def page_item_count(self, page_index):
+        if len(self.collection) // self.items_per_page > page_index:
+            return -1
+        elif len(self.collection) % self.items_per_page == 0:
+        else:
+            return len(self.collection) - len(self.collection) // self.items_per_page * self.items_per_page
+
+
+    # determines what page an item is on. Zero based indexes.
+    # this method should return -1 for item_index values that are out of range
+    def page_index(self, item_index):
         pass
 
-    # add method - given two numbers, return the addition
-    def add(self, x1, x2):
-        return x1
 
-    # multiply method - given two numbers, return the
-    # multiplication of the two
-    def multiply(self, x1, x2):
-        return x1 * x2
 
-    # subtract method - given two numbers, return the value
-    # of first value minus the second
-    def subtract(self, x1, x2):
-        return x1 - x2
-
-    # divide method - given two numbers, return the value
-    # of first value divided by the second
-    def divide(self, x1, x2):
-        if x2 != 0:
-            return x1 / x2
+if __name__ == '__main__':
+    # solution(1947)
+    print(1111)
