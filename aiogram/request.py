1e49bde2f2dc0996356251f0a16e78179be3ff39
ocitkowski <o.citkowski@gmail.com>
2022-12-24 09:55:32 +0100

    added +

--- aiogram/request.py
+++ aiogram/request.py
@@ -11,7 +11,23 @@ authorization = {"Authorization": BearerTOKEN_API}
 payload = {'limit': '10', 'offset': '10'}
 
 
-if __name__ == '__main__':
+import asyncio, time
 
-    for i in requests.get(url=url, params=payload, headers=authorization):
-        print(i)
+
+
+async def my_function_sec(sec: int):
+    await asyncio.sleep(sec)
+    print(f'Пройшлo {sec} сек.')
+
+
+async def main():
+    task_1 = asyncio.create_task(my_function_sec(1))
+    task_2 = asyncio.create_task(my_function_sec(5))
+
+    await task_1
+    await task_2
+
+
+if __name__ == '__main__':
+    for i in range(10):
+        asyncio.run(main())
