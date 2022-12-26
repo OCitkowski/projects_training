89e32431fd11c0b40cb00c8b4741ebdfce220272
ocitkowski <o.citkowski@gmail.com>
2022-12-26 10:19:49 +0100

    added +++

--- aiogram/request.py
+++ aiogram/request.py
@@ -2,20 +2,23 @@ from dotenv import load_dotenv
 import asyncio, time, os, requests
 
 load_dotenv()
-TOKEN_API = os.getenv('TOKEN_API')
+
 url = "https://ocitkowski.pythonanywhere.com/api/v1/notes/"
 
+TOKEN_API = os.getenv('TOKEN_API')
 BearerTOKEN_API = f"Bearer {TOKEN_API}"
 authorization = {"Authorization": BearerTOKEN_API}
-payload = {'limit': '10', 'offset': '10'}
+params = {'limit': '10', 'offset': '10'}
 
-#
-#
-# async def my_function_sec(sec: int):
-#     await asyncio.sleep(sec)
-#     print(f'Пройшлo {sec} сек.')
-#
-#
+# params = {}
+# params.limit = '10'
+# params.offset = '10'
+# params.Authorization = BearerTOKEN_API
+
+
+TOKEN_API = '82603ae1dd2d9c22a63309297103a261b1923369'
+
+
 # async def main():
 #     task_1 = asyncio.create_task(my_function_sec(1))
 #     task_2 = asyncio.create_task(my_function_sec(5))
@@ -24,9 +27,20 @@ payload = {'limit': '10', 'offset': '10'}
 #     await task_2
 
 
-
 if __name__ == '__main__':
-    for i in range(10):
-        asyncio.run(main())
+    # for i in range(10):
+        # asyncio.run(main())
+    response = requests.get(url, headers=authorization, params=params).json()
 
-        r = requests.get()
+    print(response)
+
+
+# class GetDataByURL:
+#     def __init__(self, url, params):
+#         self.TOKEN_API = os.getenv('TOKEN_API')
+#         self.url = url
+#         self.params = params
+#
+#     async def asyncio_get(self):
+#         task = asyncio.create_task(requests.get(self.url, headers={self.authorization, params}))
+#         return await task
