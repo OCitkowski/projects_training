b708be3c47dbd1b01efa6513b6046feb2b36fe31
fox <o.citkowski@gmail.com>
2023-01-17 19:33:31 +0100

    11.01.23.001 mate end

--- exercise_mate/added.py
+++ exercise_mate/added.py
@@ -29,6 +29,7 @@ def scrolling_text(string: str) -> list:
         list_string.append(x)
     return result
 
+
 def check_number(number: int) -> list:
     result = []
 
@@ -50,18 +51,104 @@ def check_number(number: int) -> list:
     return result
 
 
+def combine_lists(ls1: list, ls2: list) -> list:
+    ls = []
+    for i in range(len(ls1)):
+        ls.append(ls1[i] + ls2[i])
+    return ls
+
+
+def is_jumping(number: int) -> str:
+    numbers_list = list(str(number))
+    result = 'JUMPING'
+    for i in range(len(numbers_list)):
+        if i == 0:
+            continue
+        x = int(numbers_list[i])
+        x1 = int(numbers_list[i - 1])
+
+        if (x - x1) == -1 or (x - x1) == 1:
+            continue
+        else:
+            result = 'NOT JUMPING'
+            break
+
+    return result
+
+
+def is_special_number(number: int) -> str:
+    numbers_list = list(str(number))
+    result = "Special!!"
+    for i in range(len(numbers_list)):
+        if int(numbers_list[i]) in (0, 1, 2, 3, 4, 5):
+            continue
+        else:
+            result = "NOT!!"
+            break
+    return result
+
+
+def is_tidy(number: int) -> bool:
+    numbers_list = list(str(number))
+    result = True
+    for i in range(len(numbers_list)):
+        if i == 0:
+            continue
+        x = int(numbers_list[i])
+        x1 = int(numbers_list[i - 1])
+
+        if x >= x1:
+            continue
+        else:
+            result = False
+            break
+
+    return result
+
+def get_largest_expression_result(a, b):
+
+    # the_bigest = []
+    # the_bigest.append(a + b)
+    # the_bigest.append(a - b)
+    # the_bigest.append(a * b)
+    # the_bigest.append(a / b)
+    #
+    # the_bigest = sorted(the_bigest)
+    #
+    # return the_bigest[3]
+
+    for i in range('klghjkghjl'):
+        print(i)
+
+
+
 
 
 if __name__ == '__main__':
-    print(check_number(3))  # [True, False, False]
-    print(check_number(10))  # [True, True, True]
-    print(check_number(0))  # [False, True, True]
-    print(check_number(-1)) # [False, False, False]
+    print(get_largest_expression_result(10, 5))  # 50
+    print(get_largest_expression_result(10, -5))  # 15
+# # Число з однієї цифри
+# print(is_jumping(98))  # "JUMPING"
+# # 7 і 9 відрізняються на 2, а не на 1
+# print(is_jumping(78))  # "NOT JUMPING"
+# # Різниця між однаковими цифрами дорівнює 0
+# print(is_jumping(7889))  # "NOT JUMPING"
+# # Усі сусідні цифри відрізняються на 1
+# print(is_jumping(23454))  # "JUMPING
 
-    # print(split_string("123456"))  # ["12", "34", "56"]
-    # print(split_string("ab cd ef"))  # ["ab", " c", "d ", "ef"]
-    # print(split_string("abc"))  # ["ab", "c_"]
-    # print(split_string(" "))  # [" _"]
-    # print(split_string(""))  #
+# print(combine_lists([1, 2, 5], [3, 6, 1]))  # [4, 8, 6]
+# print(combine_lists([1], [6]))  # [7]
+# print(combine_lists([], [])) # []
 
-    # print(scrolling_text('robot'))
+# print(check_number(3))  # [True, False, False]
+# print(check_number(10))  # [True, True, True]
+# print(check_number(0))  # [False, True, True]
+# print(check_number(-1)) # [False, False, False]
+
+# print(split_string("123456"))  # ["12", "34", "56"]
+# print(split_string("ab cd ef"))  # ["ab", " c", "d ", "ef"]
+# print(split_string("abc"))  # ["ab", "c_"]
+# print(split_string(" "))  # [" _"]
+# print(split_string(""))  #
+
+# print(scrolling_text('robot'))
