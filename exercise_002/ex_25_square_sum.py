58033114894d622049389b47c3fd78f3f623d8ab
fox <o.citkowski@gmail.com>
2023-01-18 14:38:47 +0100

    18.01.23.001 codewars 25 end

--- exercise_002/ex_25_square_sum.py
+++ exercise_002/ex_25_square_sum.py
@@ -1,10 +1,15 @@
 def square_sum(numbers):
-    return 5
+    result = 0
+    for i in numbers:
+        result += i*i
+    return result
 
+
+# def square_sum(numbers):
+#     return sum(x ** 2 for x in numbers)
 
 if __name__ == '__main__':
-
-    assert(square_sum([1, 2]) == 5)
+    assert(square_sum([1, 2]) == 6)
     assert(square_sum([0, 3, 4, 5]) == 50)
     assert(square_sum([]) == 0)
     assert(square_sum([-1, -2]) == 5)
--- tests/test_25_square_sum..py
+++ tests/test_25_square_sum..py
@@ -1,13 +1,16 @@
 from exercise_002.ex_25_square_sum import square_sum
-from unittest import TestCase
+import unittest
 
 
-class TestSquareSum(TestCase):
+class TestSquareSum(unittest.TestCase):
 
     def test_unique(self):
+        self.assertEqual(square_sum([1, 2]), 5)
+        self.assertEqual(square_sum([0, 3, 4, 5]), 50)
+        self.assertEqual(square_sum([]), 0)
+        self.assertEqual(square_sum([-1, -2]), 5)
+        self.assertEqual(square_sum([-1, 0, 1]), 2)
 
-        self.assertEqual(square_sum([1, 2]) , 5)
-        self.assertEqual(square_sum([0, 3, 4, 5]) , 50)
-        self.assertEqual(square_sum([]) == 0)
-        self.assertEqual(square_sum([-1, -2]) , 5)
-        self.assertEqual(square_sum([-1, 0, 1]) , 2)
+
+if __name__ == '__main__':
+    unittest.main()
